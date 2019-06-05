456608ac16ef76d57710d0d54604fd02fc19ae66
Lea <lea@tumblr.com>
2019-06-05 16:02:15 -0400

    new structure of files

--- src/detail.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class DetailView extends Component {
-  render() {
-    return <h1>This is our detail view{this.props.id}</h1>;
-  }
-}
-
-export default DetailView;
